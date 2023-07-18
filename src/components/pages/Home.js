73b0811521ee082103875e54055b0151bd0ade24
Marcus Walker <mwalke35@yahoo.com>
2023-07-18 10:54:08 -0400

    added about me update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>About Marcus</h1>
       <p>
         <img className="img-fluid mx-auto d-block" src={require("../images/marcus profile pic.jpg")} width="450" height="350" alt="" /> 
-        A highly dedicated and growth-focused professional with a recently completed MERN Full Stack web development program Committed to crafting, developing, and delivering unique, custom software solutions that solve businesses' most challenging problems using a combination of industry standard and cutting-edge tools, such as HTML, CSS, JavaScript, Node.js, Visual Studio, and Mongo DB. Strong time-management and organizational skills with a commitment to clean and efficient coding practices. Known as a versatile and quick learner, willing to embrace new challenges and eager to learn more and grow in the software engineering field. 
+        A highly dedicated and growth-focused professional with a recently completed MERN Full Stack web development program. Committed to crafting, developing, and delivering unique, custom software solutions that solve businesses' most challenging problems using a combination of industry standards, such as HTML, CSS, JavaScript, Python, Node.js, AWS, Mysql, Mongo DB, Visual Studio. Strong time-management and organizational skills with a commitment to clean and efficient coding practices. Being a versatile and quick learner, I eagerly embrace new challenges and continuously seek opportunities to expand my knowledge in the software engineering field. My commitment to growth and improvement drives me to excel in every project I undertake. Let's work together to create innovative solutions that make a real impact in the software development world!
 
 
       </p>
